0d644c28c93199971770f39dfdf32c00fd5a7ff1
Raymond Feeney <rfeeney@live.co.uk>
2026-02-06 19:00:11 +0000

    Corrected navigation

--- src/OrchidApp.Web/Models/PlantCurrentLocation.cs
+++ src/OrchidApp.Web/Models/PlantCurrentLocation.cs
@@ -2,24 +2,22 @@ namespace OrchidApp.Web.Models;
 
 public class PlantCurrentLocation
 {
-    public int PlantLocationHistoryId { get; set; }
-
     public int PlantId { get; set; }
+    public int TaxonId { get; set; }
 
     public string PlantTag { get; set; } = string.Empty;
 
     public string? PlantName { get; set; }
 
-    public int TaxonId { get; set; }
-
     public string DisplayName { get; set; } = string.Empty;
 
-    public int LocationId { get; set; }
+    public int? LocationId { get; set; }
+    public int? PlantLocationHistoryId { get; set; }
 
     public string? LocationName { get; set; } = string.Empty;
 
     public string? LocationTypeCode { get; set; } = string.Empty;
 
-    public DateTime LocationStartDateTime { get; set; }
+    public DateTime? LocationStartDateTime { get; set; }
 }
 
