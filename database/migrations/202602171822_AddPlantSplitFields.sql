c11bce8a7c4b6cb1917aeaeff5a9cff671520e0a
Raymond Feeney <rfeeney@live.co.uk>
2026-02-20 16:13:21 +0000

    Alter plantsplit

--- database/migrations/202602171822_AddPlantSplitFields.sql
+++ database/migrations/202602171822_AddPlantSplitFields.sql
@@ -7,3 +7,7 @@ ALTER TABLE orchids.plantsplit
     DEFAULT CURRENT_TIMESTAMP 
     ON UPDATE CURRENT_TIMESTAMP
     COMMENT 'Last update timestamp (local time)';
+
+ALTER TABLE orchids.plantsplit
+	ADD COLUMN isActive tinyint(1) NOT NULL DEFAULT '1' 
+		COMMENT '1 = active record; 0 = logically removed (soft delete)';
--- database/migrations/202602191755_AddPlantSplitFields.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE orchids.plantsplit
-	ADD COLUMN isActive tinyint(1) NOT NULL DEFAULT '1' 
-		COMMENT '1 = active record; 0 = logically removed (soft delete)';
