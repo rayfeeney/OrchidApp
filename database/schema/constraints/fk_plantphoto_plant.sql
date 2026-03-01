40597de27632ae3a85e8921222fb8d86e5b405d1
Raymond Feeney <rfeeney@live.co.uk>
2026-03-01 12:11:42 +0000

    Create 202603011210_AddStructuralCheckConstraints.sql

--- database/schema/constraints/fk_plantphoto_plant.sql
+++ database/schema/constraints/fk_plantphoto_plant.sql
@@ -2,6 +2,6 @@ ALTER TABLE `orchids`.`plantphoto`
   ADD CONSTRAINT `fk_plantphoto_plant`
   FOREIGN KEY (`plantId`)
   REFERENCES `orchids`.`plant` (`plantId`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
+  ON DELETE RESTRICT
+  ON UPDATE RESTRICT;
 
