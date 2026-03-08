f1928fd856fa8ca89e3194ef7821f4747806119c
Raymond Feeney <rfeeney@live.co.uk>
2026-03-08 18:34:27 +0000

    Create 202603081832_RecreateVPlantActiveSummary.sql

---
+++ database/migrations/202603081832_RecreateVPlantActiveSummary.sql
@@ -0,0 +1,29 @@
+USE orchids;
+
+SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+CREATE OR REPLACE
+VIEW `orchids`.`vplantactivesummary` AS
+    SELECT 
+        `p`.`plantId` AS `plantId`,
+        `s`.`taxonId` AS `taxonId`,
+        `p`.`plantTag` AS `plantTag`,
+        `p`.`plantName` AS `plantName`,
+        `p`.`acquisitionDate` AS `acquisitionDate`,
+        `p`.`acquisitionSource` AS `acquisitionSource`,
+        `g`.`genusName` AS `genusName`,
+        `s`.`speciesName` AS `speciesName`,
+        `s`.`hybridName` AS `hybridName`,
+        (CASE
+            WHEN (`s`.`isSystemManaged` = 1) THEN CONCAT(`g`.`genusName`, ' sp.')
+            WHEN (`s`.`speciesName` IS NOT NULL) THEN CONCAT(`g`.`genusName`, ' ', `s`.`speciesName`)
+            WHEN (`s`.`hybridName` IS NOT NULL) THEN CONCAT(`g`.`genusName`, ' ', `s`.`hybridName`)
+            ELSE `g`.`genusName`
+        END) AS `displayName`
+    FROM
+        ((`orchids`.`plant` `p`
+        JOIN `orchids`.`taxon` `s` ON ((`s`.`taxonId` = `p`.`taxonId`)))
+        JOIN `orchids`.`genus` `g` ON ((`g`.`genusId` = `s`.`genusId`)))
+    WHERE
+        ((`p`.`isActive` = 1)
+            AND (`p`.`endDate` IS NULL))
