c2280ead164d23add3ba13268bf13d877b9c4953
Raymond Feeney <rfeeney@live.co.uk>
2026-03-08 17:10:33 +0000

    Create 202603081705_RecreateVPlantActiveCurrentLocation.sql

---
+++ database/migrations/202603081705_RecreateVPlantActiveCurrentLocation.sql
@@ -0,0 +1,81 @@
+USE orchids;
+
+SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+CREATE OR REPLACE
+VIEW `orchids`.`vplantactivecurrentlocation` AS
+
+SELECT `p`.`plantId` AS `plantId`
+	,`t`.`taxonId` AS `taxonId`
+	,`p`.`plantTag` AS `plantTag`
+	,`p`.`plantName` AS `plantName`
+	,`loc`.`locationId` AS `locationId`
+	,`loc`.`locationName` AS `locationName`
+	,`loc`.`locationTypeCode` AS `locationTypeCode`
+	,`plh`.`startDateTime` AS `locationStartDateTime`
+	,(
+		CASE 
+			WHEN (`t`.`isSystemManaged` = 1)
+				THEN CONCAT (
+						`g`.`genusName`
+						,' sp.'
+						)
+			WHEN (`t`.`speciesName` IS NOT NULL)
+				THEN CONCAT (
+						`g`.`genusName`
+						,' '
+						,`t`.`speciesName`
+						)
+			WHEN (`t`.`hybridName` IS NOT NULL)
+				THEN CONCAT (
+						`g`.`genusName`
+						,' '
+						,`t`.`hybridName`
+						)
+			ELSE `g`.`genusName`
+			END
+		) AS `displayName`
+	,`pp`.`filePath` AS `heroFilePath`
+FROM (
+	(
+		(
+			(
+				(
+					`orchids`.`plant` `p` JOIN `orchids`.`taxon` `t` ON ((`t`.`taxonId` = `p`.`taxonId`))
+					) JOIN `orchids`.`genus` `g` ON ((`g`.`genusId` = `t`.`genusId`))
+				) LEFT JOIN (
+				SELECT `sub`.`plantId` AS `plantId`
+					,`sub`.`locationId` AS `locationId`
+					,`sub`.`startDateTime` AS `startDateTime`
+					,`sub`.`RowOrder` AS `RowOrder`
+				FROM (
+					SELECT `lochistory`.`plantId` AS `plantId`
+						,`lochistory`.`locationId` AS `locationId`
+						,`lochistory`.`startDateTime` AS `startDateTime`
+						,row_number() OVER (
+							PARTITION BY `lochistory`.`plantId` ORDER BY `lochistory`.`startDateTime` DESC
+							) AS `RowOrder`
+					FROM `orchids`.`plantlocationhistory` `lochistory`
+					WHERE (`lochistory`.`isActive` = 1)
+					) `sub`
+				WHERE (`sub`.`RowOrder` = 1)
+				) `plh` ON ((`p`.`plantId` = `plh`.`plantId`))
+			) LEFT JOIN `orchids`.`location` `loc` ON (
+				(
+					(`loc`.`locationId` = `plh`.`locationId`)
+					AND (`loc`.`isActive` = 1)
+					)
+				)
+		) LEFT JOIN `orchids`.`plantphoto` `pp` ON (
+			(
+				(`pp`.`plantId` = `p`.`plantId`)
+				AND (`pp`.`isHero` = 1)
+				AND (`pp`.`isActive` = 1)
+				)
+			)
+	)
+WHERE (
+		(`p`.`isActive` = 1)
+		AND (`p`.`endDate` IS NULL)
+		)
+
