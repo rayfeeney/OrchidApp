160d9f730a6fb94c5cf2f5a5e3e4243e8a798d82
Raymond Feeney <rfeeney@live.co.uk>
2026-03-10 13:25:05 +0000

    Create 202603101323_RecreateSpUpdateTaxonMetadata.sql

---
+++ database/schema/routines/spUpdateGenusMetadata.sql
@@ -0,0 +1,16 @@
+DELIMITER //
+CREATE OR REPLACE PROCEDURE `spUpdateGenusMetadata`(
+   IN p_genusId INT,
+   IN p_genusNotes TEXT,
+   IN p_isActive TINYINT
+ )
+BEGIN
+   UPDATE orchids.genus
+   SET
+     genusNotes  = p_genusNotes,
+     isActive    = p_isActive
+   WHERE genusId = p_genusId;
+ END
+//
+DELIMITER ;
+
