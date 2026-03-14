3a306f574195bd9f68b08c51c23e8c164061aa0e
Raymond Feeney <rfeeney@live.co.uk>
2026-03-14 14:09:36 +0000

    Changes

--- database/schema/routines/spAddTaxonInternal.sql
+++ database/schema/routines/spAddTaxonInternal.sql
@@ -9,34 +9,31 @@ CREATE OR REPLACE PROCEDURE `spAddTaxonInternal`(
     OUT pTaxonId            INT
 )
 BEGIN
-    DECLARE vGenusExists INT;
-    DECLARE vGenusIsActive INT;
+    DECLARE vGenusIsActive TINYINT;
 
     DECLARE vSpeciesName VARCHAR(100);
     DECLARE vHybridName  VARCHAR(150);
     DECLARE vGrowthNotes TEXT;
     DECLARE vTaxonNotes  TEXT;
 
-    
     SET vSpeciesName = NULLIF(TRIM(pSpeciesName), '');
     SET vHybridName  = NULLIF(TRIM(pHybridName), '');
     SET vGrowthNotes = NULLIF(TRIM(pGrowthNotes), '');
     SET vTaxonNotes  = NULLIF(TRIM(pTaxonNotes), '');
 
-    
-    SELECT COUNT(*), MAX(isActive)
-    INTO vGenusExists, vGenusIsActive
+    SELECT isActive
+    INTO vGenusIsActive
     FROM genus
     WHERE genusId = pGenusId;
 
-    IF vGenusExists = 0 THEN
+    IF vGenusIsActive IS NULL THEN
         SIGNAL SQLSTATE '45000'
             SET MESSAGE_TEXT = 'Invalid genusId: genus does not exist';
     END IF;
 
     IF vGenusIsActive = 0 THEN
         SIGNAL SQLSTATE '45000'
-            SET MESSAGE_TEXT = 'Cannot create taxon for inactive genus';
+            SET MESSAGE_TEXT = 'Taxon must be created under an active genus.';
     END IF;
 
     INSERT INTO taxon (
