d636af5c7c6780ba53be1b4204b6b81304368bea
Raymond Feeney <rfeeney@live.co.uk>
2026-03-19 18:16:20 +0000

    Update 202603191805_AddPlantTagAccession.sql

--- database/schema/routines/__migrate_generate_planttag.sql
+++ database/schema/routines/__migrate_generate_planttag.sql
@@ -1,9 +1,5 @@
-DROP PROCEDURE IF EXISTS __migrate_generate_planttag;
-DROP PROCEDURE IF EXISTS __migrate_backfill_planttag;
-
 DELIMITER //
-
-CREATE PROCEDURE __migrate_generate_planttag(OUT pPlantTag CHAR(8))
+CREATE OR REPLACE PROCEDURE `__migrate_generate_planttag`(OUT pPlantTag CHAR(8))
 BEGIN
     DECLARE vEntropy CHAR(64);
     DECLARE vPrefix CHAR(2);
@@ -46,16 +42,7 @@ BEGIN
         SET vDigit = CONV(SUBSTRING(vEntropy, 9, 2), 16, 10) MOD 10;
         SET vBlock = CONV(SUBSTRING(vEntropy, 11, 8), 16, 10) MOD 1000;
 
-        SET vChecksum = (
-            (
-                ASCII(SUBSTRING(vPrefix,1,1)) +
-                ASCII(SUBSTRING(vPrefix,2,1)) +
-                vDigit +
-                FLOOR(vBlock / 100) +
-                FLOOR((vBlock % 100) / 10) +
-                (vBlock % 10)
-            ) MOD 10
-        );
+        SET vChecksum = fnPlantTagChecksum(CONCAT(vPrefix, vDigit, '-', LPAD(vBlock, 3, '0')));
 
         SET vCandidate = CONCAT(
             vPrefix,
@@ -74,46 +61,7 @@ BEGIN
             LEAVE generation_loop;
         END IF;
     END LOOP;
-END//
-
-CREATE PROCEDURE __migrate_backfill_planttag()
-BEGIN
-    DECLARE vDone INT DEFAULT 0;
-    DECLARE vPlantId INT;
-    DECLARE vTag CHAR(8);
-
-    DECLARE cur CURSOR FOR
-        SELECT plantId
-        FROM plant
-        ORDER BY plantId;
-
-    DECLARE CONTINUE HANDLER FOR NOT FOUND SET vDone = 1;
-
-    OPEN cur;
-
-    read_loop: LOOP
-        FETCH cur INTO vPlantId;
-
-        IF vDone = 1 THEN
-            LEAVE read_loop;
-        END IF;
-
-        CALL __migrate_generate_planttag(vTag);
-
-        UPDATE plant
-        SET plantTag = vTag
-        WHERE plantId = vPlantId;
-    END LOOP;
-
-    CLOSE cur;
-END//
-
+END
+//
 DELIMITER ;
 
-CALL __migrate_backfill_planttag();
-
-DROP PROCEDURE __migrate_backfill_planttag;
-DROP PROCEDURE __migrate_generate_planttag;
-
-ALTER TABLE plant
-ADD CONSTRAINT uqPlantPlantTag UNIQUE (plantTag);
