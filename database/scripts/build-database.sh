0ce831f6ae1b1cf1dcb21606cc2d6ed6e7f24d33
Raymond Feeney <rfeeney@live.co.uk>
2026-03-19 15:48:54 +0000

    Update build-database.sh

--- database/scripts/build-database.sh
+++ database/scripts/build-database.sh
@@ -59,7 +59,7 @@ apply_required_dir () {
   while IFS= read -r f; do
     echo "  → $f"
     mariadb \
-      --default-character-set=utf8mb4
+      --default-character-set=utf8mb4 \
       -h "$MARIADB_HOST" \
       -P "$MARIADB_PORT" \
       -u "$MARIADB_USER" \
@@ -91,7 +91,7 @@ apply_optional_dir () {
   while IFS= read -r f; do
     echo "  → $f"
     mariadb \
-      --default-character-set=utf8mb4
+      --default-character-set=utf8mb4 \
       -h "$MARIADB_HOST" \
       -P "$MARIADB_PORT" \
       -u "$MARIADB_USER" \
