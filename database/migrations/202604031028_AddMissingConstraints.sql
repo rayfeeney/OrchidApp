649943e0a0f0e5731388ce266fcfdda70fbc1d50
Raymond Feeney <rfeeney@live.co.uk>
2026-04-03 10:30:56 +0100

    Update 202604031028_AddMissingConstraints.sql

--- database/migrations/202604031028_AddMissingConstraints.sql
+++ database/migrations/202604031028_AddMissingConstraints.sql
@@ -1,7 +1,7 @@
 ALTER TABLE plantevent
 ADD CONSTRAINT fk_plantevent_observationtype
-FOREIGN KEY (observationTypeId)
-REFERENCES observationtypeid(Id)
+FOREIGN KEY (observationType)
+REFERENCES observationtype(Id)
 ON DELETE RESTRICT
 ON UPDATE RESTRICT;
 
