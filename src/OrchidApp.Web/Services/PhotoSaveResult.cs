3c8bdec2551cae33b71ae2179b805267112ced3d
rayfeeney <rfeeney@live.co.uk>
2026-04-04 13:50:05 +0100

    Taxon photo (#204)
    * Create 202604041125_CreateTaxonPhoto.sql
    
    * Changes for current photos
    
    * Initial Taxon Photo Add
    
    * Update OrchidDbContext.cs
    
    * Amended Taxa.Details
    
    * Update Details.cshtml
    
    * Update Details.cshtml
    
    * Update Details.cshtml
    
    * Taxon Edit
    
    * Thumbnail

--- src/OrchidApp.Web/Services/PhotoSaveResult.cs
+++ src/OrchidApp.Web/Services/PhotoSaveResult.cs
@@ -3,6 +3,7 @@ namespace OrchidApp.Web.Services;
 public sealed class PhotoSaveResult
 {
     public string RelativePath { get; init; } = "";
+    public string ThumbnailRelativePath { get; init; } = "";
     public string MimeType { get; init; } = "";
     public int Width { get; init; }
     public int Height { get; init; }
