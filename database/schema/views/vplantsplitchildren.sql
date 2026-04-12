f81e707bd40f5e652855bd7c6a34e26c66bf41a4
rayfeeney <rfeeney@live.co.uk>
2026-04-12 14:42:35 +0100

    Update site.css (#228)

--- database/schema/views/vplantsplitchildren.sql
+++ database/schema/views/vplantsplitchildren.sql
@@ -1,2 +1,2 @@
-CREATE OR REPLACE VIEW `vplantsplitchildren` AS select `ps`.`parentPlantId` AS `parentPlantId`,`child`.`plantId` AS `childPlantId`,`child`.`plantTag` AS `plantTag`,`child`.`acquisitionDate` AS `acquisitionDate` from ((`plantsplit` `ps` join `plantsplitchild` `psc` on((`psc`.`plantSplitId` = `ps`.`plantSplitId`))) join `vplantstatus` `child` on((`child`.`plantId` = `psc`.`childPlantId`)));
+CREATE OR REPLACE VIEW `vplantsplitchildren` AS select `ps`.`parentPlantId` AS `parentPlantId`,`child`.`plantId` AS `childPlantId`,`child`.`plantTag` AS `plantTag`,`child`.`acquisitionDate` AS `acquisitionDate` from ((`plantsplit` `ps` join `plantsplitchild` `psc` on((`psc`.`plantSplitId` = `ps`.`plantSplitId`))) join `plant` `child` on((`child`.`plantId` = `psc`.`childPlantId`)));
 
