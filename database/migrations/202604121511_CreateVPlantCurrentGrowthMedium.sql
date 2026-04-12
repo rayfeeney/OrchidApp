1a298b9a75edb7541f3fdd5e37fa139664cc3d91
Raymond Feeney <rfeeney@live.co.uk>
2026-04-12 15:26:19 +0100

    Repotting growthmedium changes

---
+++ database/migrations/202604121511_CreateVPlantCurrentGrowthMedium.sql
@@ -0,0 +1,26 @@
+CREATE OR REPLACE VIEW vplantcurrentgrowthmedium AS
+SELECT
+    r.plantId
+    ,r.newGrowthMediumId
+    ,gm.name AS growthMediumName
+    ,r.potSize
+    ,r.repottingNotes
+    ,r.repotDate
+FROM (
+    SELECT
+        repottingId,
+        plantId,
+        newGrowthMediumId,
+        potSize,
+        repottingNotes,
+        repotDate,
+        ROW_NUMBER() OVER (
+            PARTITION BY plantId
+            ORDER BY repotDate DESC, repottingId DESC
+        ) AS rn
+    FROM repotting
+    WHERE isActive = 1
+) r
+LEFT JOIN growthmedium gm
+    ON gm.growthMediumId = r.newGrowthMediumId
+WHERE r.rn = 1;
