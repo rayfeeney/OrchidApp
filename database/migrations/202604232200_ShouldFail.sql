54a464d9484fa2bc183a61b11f06f6047cddef80
Raymond Feeney <rfeeney@live.co.uk>
2026-04-23 22:26:19 +0100

    Create 202604232200_ShouldFail.sql

---
+++ database/migrations/202604232200_ShouldFail.sql
@@ -0,0 +1,3 @@
+CREATE TABLE IF NOT EXISTS ShouldFail (
+    id INT PRIMARY KEY
+);
---
+++ database/schema/tables/testoutoforder.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS `testoutoforder` (
+  `id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+
+) ENGINE=InnoDB;
+
