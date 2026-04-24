9085fed43103607db9e7f8bb26724f52f84605ff
Raymond Feeney <rfeeney@live.co.uk>
2026-04-24 20:26:51 +0100

    Update 202604242021_AlterPlantEvent.sql

--- database/migrations/202604242021_AlterPlantEvent.sql
+++ database/migrations/202604242021_AlterPlantEvent.sql
@@ -1,2 +1,14 @@
+-- 1. Drop FK
 ALTER TABLE plantevent
-DROP INDEX idx_plantevent_observationTypeId;
+DROP FOREIGN KEY fk_plantevent_observationtype;
+
+-- 2. Drop index
+DROP INDEX idx_plantevent_observationTypeId ON plantevent;
+
+-- 3. Recreate FK (let MariaDB create index automatically)
+ALTER TABLE plantevent
+ADD CONSTRAINT fk_plantevent_observationtype
+FOREIGN KEY (observationTypeId)
+REFERENCES observationtype (Id)
+ON DELETE RESTRICT
+ON UPDATE RESTRICT;
