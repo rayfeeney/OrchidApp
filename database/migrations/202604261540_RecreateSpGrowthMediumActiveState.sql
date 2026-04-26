f674194609ea528920384beef098fde98e0e6b28
Raymond Feeney <rfeeney@live.co.uk>
2026-04-26 15:41:47 +0100

    Create 202604261540_RecreateSpGrowthMediumActiveState.sql

---
+++ database/migrations/202604261540_RecreateSpGrowthMediumActiveState.sql
@@ -0,0 +1,28 @@
+DROP PROCEDURE IF EXISTS spSetGrowthMediumActiveState;
+
+DELIMITER //
+
+CREATE PROCEDURE spSetGrowthMediumActiveState(
+    IN pGrowthMediumId INT,
+    IN pIsActive BOOLEAN
+)
+proc: BEGIN
+
+    DECLARE vCurrentState BOOLEAN;
+
+    SELECT isActive
+    INTO vCurrentState
+    FROM growthmedium
+    WHERE growthMediumId = pGrowthMediumId;
+
+    IF vCurrentState = pIsActive THEN
+        LEAVE proc;
+    END IF;
+
+    UPDATE growthmedium
+    SET isActive = pIsActive
+    WHERE growthMediumId = pGrowthMediumId;
+
+END //
+
+DELIMITER ;
