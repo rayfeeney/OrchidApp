fb9ed9dfa6e7bb54a686c6300fe7f68b9c82b583
rayfeeney <rfeeney@live.co.uk>
2026-05-04 10:18:00 +0100

    Cleanup (#257)

--- database/tests/structure/location_Unique.sql
+++
@@ -1,7 +0,0 @@
--- location_Unique.sql
-
--- Location has no uniqueness constraints beyond the primary key.
--- This is a deliberate V1 design decision.
--- No test required.
-
--- This will be changed in a later version.
