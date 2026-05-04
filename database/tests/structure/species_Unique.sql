7c9d65b74a1e5f8f73045a71dbf0943b9d357585
Raymond Feeney <rfeeney@live.co.uk>
2026-05-04 10:16:25 +0100

    Cleanup

--- database/tests/structure/species_Unique.sql
+++
@@ -1,7 +0,0 @@
--- species_Unique.sql
-
--- Species has no uniqueness constraints beyond the primary key.
--- This is a deliberate V1 design decision.
--- No test required.
-
--- This will be changed in a later version.
